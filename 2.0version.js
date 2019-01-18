2ddb931d5f88b2ba3c162210338914e8e39eeed2
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-01-18 01:00:14 +0000

    learned the very basics of functions

---
+++ 2.0version.js
@@ -0,0 +1,22 @@
+function sayHiTo(person){
+	console.log("hi", person);
+}
+//person = "Gordon"
+sayHiTo("Gordon");
+
+
+// function makeSandwchWith(filling){
+// 	Get one slice of bread;
+// 	Add _____;
+// 	Put a slice of bread on top;
+// }
+// makeTurkeySandwish();
+
+// makeSandwchWith ______
+// Get one slice of bread;
+// 	Add _____;
+// 	Put a slice of bread on top;
+	
+	
+// 	makeSandwchWith(ham);
+// console.log()
