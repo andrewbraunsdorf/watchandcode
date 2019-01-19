34b1e454d16596418294467d054f62e6496c16ff
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-01-19 21:13:56 +0000

    created a method

---
+++ 2.0interlude.js
@@ -0,0 +1,11 @@
+var myName = "Andrew";
+
+function sayNAme(){
+	var secret = "watchandcode";
+	console.log(myName);
+}
+
+sayNAme();
+console.log(secret);
+
+//if youre inside of a function, you can look out and see data, but the opposite isn't true.  If you're outisde, you can't look in.
---
+++ 3.0version.js
@@ -0,0 +1,15 @@
+var andrew = {
+	name: "Andrew",
+	sayName: function(){
+		console.log(this.name);
+	}
+};
+andrew.sayName();
+
+
+// var myComputer = {
+// 	operatingSystem: "mac",
+// 	screenSize: "15 inches",
+// 	purchaseYear: 2011
+// };
+// myComputer.operatingSystem;
