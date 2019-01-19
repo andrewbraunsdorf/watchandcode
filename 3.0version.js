6b9d8d8943ed4e996bed81292a85ae199e87b985
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-01-19 21:28:17 +0000

    added methods and called methods with this

--- 3.0version.js
+++ 3.0version.js
@@ -1,10 +1,27 @@
-var andrew = {
-	name: "Andrew",
-	sayName: function(){
-		console.log(this.name);
+
+var todoList = {
+	todos: ["item 1", "item 2", "item 3"],
+	displayTodos: function(){
+		console.log("My Todos:", this.todos);
+	},
+	addTodo: function(todo) {
+		this.todos.push(todo);
+		this.displayTodos();
 	}
 };
-andrew.sayName();
+
+// function addTodo(todo) {
+// 	todos.push(todo);
+// 	displayTodos();
+// }
+
+// var andrew = {
+// 	name: "Andrew",
+// 	sayName: function(){
+// 		console.log(this.name);
+// 	}
+// };
+// andrew.sayName();
 
 
 // var myComputer = {
