18f5760c1b78d5afedb0dd46ae1fdc2d07e63c38
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-01-20 22:58:29 +0000

    created if and else statements in the method

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 var todoList = {
 	todos: [],
-	displayTodos: function(){
-		console.log("My Todos:");
-		for (var i = 0; i < this.todos.length; i++) {
-			console.log(this.todos[i].todoText);
+	displayTodos: function() {
+		if (this.todos.length === 0) {
+			console.log("Your todo list is empty!");
+		}
+		else {
+			console.log("My Todos:");
+			for (var i = 0; i < this.todos.length; i++) {
+				console.log(this.todos[i].todoText);
+			}
 		}
 	},
-	addTodo: function(todoText) { 
+	addTodo: function(todoText) {
 		this.todos.push({
 			todoText: todoText,
 			completed: false
@@ -22,7 +27,7 @@ var todoList = {
 		this.todos.splice(position, 1);
 		this.displayTodos();
 	},
-	toggleCompleted: function(position){
+	toggleCompleted: function(position) {
 		var todo = this.todos[position];
 		todo.completed = !todo.completed;
 		this.displayTodos();
