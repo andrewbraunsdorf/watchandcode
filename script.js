5fb298f947a77dc0e64c3bdaf1a6358f9cc144a3
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-01-20 21:44:54 +0000

    added a method where the Boolean changes for the opposite effect by putting ! in front of it.

--- script.js
+++ script.js
@@ -18,5 +18,10 @@ var todoList = {
 	deleteTodo: function(position) {
 		this.todos.splice(position, 1);
 		this.displayTodos();
+	},
+	toggleCompleted: function(position){
+		var todo = this.todos[position];
+		todo.completed = !todo.completed;
+		this.displayTodos();
 	}
 };
