cde88def895021564a1811f650599b0c29015396
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-01-20 22:04:29 +0000

    started the basics of for loops

---
+++ test.js
@@ -0,0 +1,18 @@
+// i = 0				// initialization
+// say "hey" if i <3		// condition
+// Increase i by 1		// final-expression
+
+// 0 "hey"
+// 1 "hey"
+// 2 "hey"
+// 3
+
+// for (initialization, condition, final-expression){
+// 	console.log("hey");
+// }
+
+for (var i = 0; i < 3; i++){
+	console.log("hey");
+}
+var testArray =["item 1", "item 2", "item 3"];
+//(i = i +1) = i++
