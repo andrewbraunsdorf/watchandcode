193a0e4ffee6def99390ad3e8da33c83a348165d
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-01-21 22:41:55 +0000

    learning the difference between objects and primitive

--- test.js
+++ test.js
@@ -1,20 +1,30 @@
-// i = 0				// initialization
-// say "hey" if i <3		// condition
-// Increase i by 1		// final-expression
 
-// 0 "hey"
-// 1 "hey"
-// 2 "hey"
-// 3
+var myHouse = {color: "blue"};
+myHouse.color = "red";
+//my house will be blue but stored in Memory address 1.  myHouse.color will go to myHouse and the memory address and then change the color.
 
-//(i = i +1) = i++
 
-// for (initialization, condition, final-expression){
-// 	console.log("hey");
+// var myPrimitive = 10;
+// var myObject = {name: "gordon"};
+// // memory address is stored in myObject
+
+// // i = 0				// initialization
+// // say "hey" if i <3		// condition
+// // Increase i by 1		// final-expression
+
+// // 0 "hey"
+// // 1 "hey"
+// // 2 "hey"
+// // 3
+
+// //(i = i +1) = i++
+
+// // for (initialization, condition, final-expression){
+// // 	console.log("hey");
+// // }
+
+// var testArray =["item 1", "item 2", "item 3", "extra item"];
+// for (var i = 0; i < testArray.length; i++){
+// 	console.log(testArray[i]);
 // }
-
-var testArray =["item 1", "item 2", "item 3", "extra item"];
-for (var i = 0; i < testArray.length; i++){
-	console.log(testArray[i]);
-}
 
