5a041b01e70ad4bd8eaaa1393c20d45ae173deb3
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-01-22 01:35:36 +0000

    fixed code from version6.0 and learned DOM

--- script.js
+++ script.js
@@ -51,8 +51,22 @@ var todoList = {
 		if (completedTodos === totalTodos) {
 			for (var i = 0; i < totalTodos; i++) {
 				this.todos[i].completed = false;
-			}
-		}
-		this.displayTodos();
+			} 
+		} else {
+				for (var i = 0; i <totalTodos; i++) {
+					this.todos[i].completed = true;
+				}
+	}
+	this.displayTodos();
 	}
 };
+
+var displayTodosButton = document.getElementById("displayTodosButton");
+var toggleAllButton = document.getElementById("toggleAllButton");
+displayTodosButton.addEventListener("click", function() {
+	todoList.displayTodos();
+});
+
+toggleAllButton.addEventListener("click", function(){
+	todoList.toggleAll();
+});
