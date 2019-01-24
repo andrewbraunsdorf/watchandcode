edf23c44a6b6c4f1e2d07694d55c87fd20114581
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-01-24 04:31:00 +0000

    added a .textContent to the for loop enabling the text of the todo item to appear on the html page.

--- script.js
+++ script.js
@@ -97,9 +97,9 @@ var view = {
 	displayTodos: function() {
 		var todosUl = document.querySelector("ul");
 		todosUl.innerHTML = "";
-	for (var i = 0; i <todoList.todos.length; i++) {
-		
+	for (var i = 0; i < todoList.todos.length; i++) {
 		var todoLi = document.createElement("li");
+		todoLi.textContent = todoList.todos[i].todoText;
 		todosUl.appendChild(todoLi);	
 		}
 	}
